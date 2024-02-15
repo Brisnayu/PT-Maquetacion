5760bf3791f6348b681ed41c88d53ff6a0ef3bd7
Brisna A. Paez M <124035708+Brisnayu@users.noreply.github.com>
2024-02-15 20:29:39 +0100

    Footer and styles footer

--- 01-maquetacion/src/components/Footer/Footer.ts
+++ 01-maquetacion/src/components/Footer/Footer.ts
@@ -6,7 +6,32 @@ export const Footer = () => {
   return `
     <div class="containerPrincipal">
       <div class="containerForm">
-        <h1>Adios</h1>
+        <img src="/logo.png" alt="logo" />
+        
+        <label class="label-follow">
+        <input class="input-follow" type="text" id="hamburger" />
+        <img src="/arrow.png" alt="logo arrow" />
+        </label>
+        
+
+        <div class="containerRRSS">
+          <h4>Follow Us</h4>
+          <div>
+          
+            <a href="mailto:brisnapaez25@gmail.com" target="_blank">
+              <img src="/rrss/logo_email.png" alt="logo email" />
+            </a>
+            <a href="https://github.com/Brisnayu" target="_blank">
+              <img src="/rrss/logo_github.png" alt="logo github" />
+            </a>
+            <a href="https://www.linkedin.com/in/brisna-a-paez-m-283934154" target="_blank">
+              <img src="/rrss/logo_linkedin.png" alt="logo linkedin" />
+            </a>
+      
+          </div>
+        </div>
+
+        
       </div>
 
       <div class="containerInformation">
